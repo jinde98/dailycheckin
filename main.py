54c47c92253e3d2c674b5d2ef24cdc827458b18c
jinde98 <127750182+jinde98@users.noreply.github.com>
2024-01-14 14:37:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import youdao , aliyunpan, tianyiyunpan
 
 def run():
     content =''
-    tilte = ""
+    title = ""
     if youdao_cookie != None:
         content = youdao.signin(youdao_cookie) +'\n\n'
-        tilte = "【有道】"
+        title = "【有道】"
     if ali_refresh_token != None :
         access_token, _ = aliyunpan.get_access_token(ali_refresh_token)
         content += aliyunpan.sign_in(access_token) + '\n\n'
-        tilte += "【阿里】"
+        title += "【阿里】"
     if ty_user != None and ty_pwd != None:
         content += tianyiyunpan.main(ty_user, ty_pwd)
-        tilte += "【天翼】"
+        title += "【天翼】"
 
     send = MessageSend()
     send.send_all(message_tokens,title+'每日签到',content)
