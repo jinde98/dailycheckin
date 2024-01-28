aefc6c85d3651229a483771e68614c9262cbc87c
jinde98 <127750182+jinde98@users.noreply.github.com>
2024-01-28 22:43:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def run():
     content =''
     title = ""
     if youdao_user != None:
-        youdao=YouDao_user_login.Youdao(youdao_user,redis_info).run
-        content= youdao.run() + '\n\n'
+        youdao_sign=YouDao_user_login.Youdao(youdao_user,redis_info).run
+        content= youdao_sign.run() + '\n\n'
     # if youdao_cookie != None:
     #     content = youdao.signin(youdao_cookie) +'\n\n'
         title = "【有道】"
