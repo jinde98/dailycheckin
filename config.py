57b8a199f65158507f66f8f6b1a04e27197f897c
jinde98 <127750182+jinde98@users.noreply.github.com>
2024-02-03 11:38:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,21 +7,12 @@ youdao_user = os.environ.get('YOUDAO_USER')
 redis_info = os.environ.get('REDIS_INFO')
 youdao_cookie = os.environ.get('YOUDAO_COOKIE')
 
-
-pushplus_token = os.environ.get('PUSHPLUS_TOKEN') 
-serverChan_sendkey = os.environ.get('SERVERCHAN_SENDKEY')
-weCom_tokens = os.environ.get('WECOM_TOKENS')
-weCom_webhook = os.environ.get('WECOM_WEBHOOK')
-bark_deviceKey = os.environ.get('BARK_DEVICEKEY')
-feishu_deviceKey = os.environ.get('FEISHU_DEVICEKEY')
-telegram_token = os.environ.get('TELEGRAM_TOKEN')
-
 message_tokens = {
-    'pushplus_token': pushplus_token,
-    'serverChan_token': serverChan_sendkey,
-    'weCom_tokens': weCom_tokens,
-    'weCom_webhook': weCom_webhook,
-    'bark_deviceKey': bark_deviceKey,
-    'feishu_deviceKey': feishu_deviceKey,
-    'telegram_token': telegram_token,
+    'pushplus_token': os.environ.get('PUSHPLUS_TOKEN') ,
+    'serverChan_token': os.environ.get('SERVERCHAN_SENDKEY'),
+    'weCom_tokens': os.environ.get('WECOM_TOKENS'),
+    'weCom_webhook': os.environ.get('WECOM_WEBHOOK'),
+    'bark_deviceKey': os.environ.get('BARK_DEVICEKEY'),
+    'feishu_deviceKey': os.environ.get('FEISHU_DEVICEKEY'),
+    'telegram_token': os.environ.get('TELEGRAM_TOKEN'),
 }
